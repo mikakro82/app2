68829adee5eac890fdb41c971e899e3355d42531
mikakro82 <michael.k@goldmail.de>
2025-07-24 09:04:07 +0200

    Update Dax.py

--- Dax.py
+++ Dax.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
         from strategy_fvg_xdax_l_full_extended import get_dax_etf_xdax as get_dax_etf_xdax_once, evaluate_fvg_strategy
         df = get_dax_etf_xdax_once()
         evaluate_fvg_strategy()
-        run_with_monitoring(df)
+        run_with_monitoring()
         time.sleep(60)
         shutdown_app()
     else:
